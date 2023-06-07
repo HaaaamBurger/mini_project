117d06981192036b477e04f4c64c866d37f77044
HaaaamBurger <gemasclashes@gmail.com>
2023-06-07 19:27:13 +0300

    #

--- main/post_details/post-details.js
+++ main/post_details/post-details.js
@@ -12,15 +12,18 @@ const userData = usersData.find(user => user.id === postId);
 const userExlorer = (user) => {
     const listData = document.getElementsByClassName('listData')[0];
 
-
     for (let field in user) {
-        const li = document.createElement('li');
-        li.innerText = `${field} - ${user[field]}`;
-        listData.appendChild(li);
+        const p = document.createElement('p');
+        p.innerText = `${field} - ${user[field]}`;
+        listData.appendChild(p);
 
         if (typeof user[field] === 'object') {
-            li.innerText = `${field}: `;
-            listData.appendChild(li);
+            const div = document.createElement('div');
+            div.classList.add('decorationalDiv');
+            p.innerText = `${field}: ⤵`;
+            p.style.marginTop = '5px';
+            div.appendChild(p);
+            listData.appendChild(div);
 
             userExlorer(user[field]);
         }
